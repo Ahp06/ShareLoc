471531adf4732fb4b115ae3a340f9ac6ab52d20c
Ahp06 <axno@hotmail.fr>
2018-12-06 21:40:49 +0100

    Configured web app

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -2,11 +2,6 @@ package dao;
 
 import model.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
-
 public class UserDao extends AbstractDao<User>{
 
     public UserDao() {
